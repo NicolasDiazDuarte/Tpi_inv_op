fb450304f1baedc651db87cdfb81588f3d552887
Selene Rouvier <selenerouvier25@outlook.com>
2020-07-31 19:03:17 -0300

    agregando cambios de pagina principal

--- rutaMasCorta/models.py
+++ rutaMasCorta/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
-# Create your models here.
-
+#Numero de veces que se va a instanciar un nodo con otro agregandole un peso
 class InstancesNumber(models.Model):
-    instancesNro= models.IntegerField(max_length=2)
+    instancesNro = models.IntegerField(max_length=2)
 
-    
+#Clase que maneja el nodo origen cargado
+class Source(models.Model):
+    source = models.CharField(max_length=2)
+
+    def __str__(self):
+        return '{}'.format(self.source)
+
+#Clase que maneja el nodo destino cargado
+class Target(models.Model):
+    target = models.CharField(max_length=2)
+
+    def __str__(self):
+        return '{}'.format(self.target)
+
+#Clase que maneja el peso de la relacion entre el nodo origen y destino cargado
+class Weight(models.Model):
+    weightNro = models.IntegerField(max_length=2)
+
+    def __str__(self):
+        return '{}'.format(self.weightNro)

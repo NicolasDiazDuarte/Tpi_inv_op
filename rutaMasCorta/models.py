0db742910cfd29c12f5a7358e5d3b70cb6f77249
Selene Rouvier <selenerouvier25@outlook.com>
2020-07-31 02:37:14 -0300

    agregando archivos de views

--- rutaMasCorta/models.py
+++ rutaMasCorta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class InstancesNumber(models.Model):
+    instancesNro= models.IntegerField(max_length=2)
+
+    
--- rutaMasCorta/views.py
+++ rutaMasCorta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={},
+    )

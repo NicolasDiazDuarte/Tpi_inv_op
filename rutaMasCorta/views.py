88f975c32ccf17fe14441c448f847b079bd5781e
NicolasDiazDuarte <niko476678@gmail.com>
2020-08-02 23:22:26 -0300

    haciendo la funcion que agrega los arreglos de nodos

--- rutaMasCorta/views.py
+++ rutaMasCorta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import TargetForm,SourceForm,WeightForm
+from .models import Source,Target,Weight
 
 # Create your views here.
 
@@ -13,14 +14,53 @@ def index(request):
         context={},
     )
 
+def instanciarArreglos(flag):
+    global arrNodoOrigen
+    global arrNodoDestino
+    global arrPeso
+    
+    arrNodoDestino = []
+    arrNodoOrigen = []
+    arrPeso = []
+    flag = False
+
+    
+        
+        
+def CargarArreglos():
+    print('todo good')
+    #usar try catch 
+    # if exists 
+    #probar con base de datos primero sino solo con arreglos 
+
 def instanciarNodos(request):
+    global arrNodoOrigen
+    global arrNodoDestino
+    global arrPeso
+    global flag
+    
+    flag=True
+
     if request.method== 'POST':
         source_form = SourceForm(request.POST)
         target_form = TargetForm(request.POST)
         weight_form = WeightForm(request.POST)
-        if source_form.is_valid() and target_form.is_valid() and weight_form.is_valid():
-            print('esta bien')
-            return redirect('rutaMasCorta.html')
+    if flag == True:
+        instanciarArreglos(flag)
+        
+       
+    if source_form.is_valid() and target_form.is_valid() and weight_form.is_valid():
+        for key,value in request.POST.items():
+            if key != 'csrfmiddlewaretoken':
+                if key == "peso" :
+                    arrPeso.append(value)
+                else:
+                    if key == "arrNodoOrigen":
+                        arrNodoOrigen.append(value)
+                    else:
+                        arrNodoDestino.append(value)
+        print(arrPeso)        
+        return render(request,'rutaMasCorta.html',{'source_form':source_form,'target_form':target_form,'weight_form':weight_form})
     else:
         source_form=SourceForm()
         target_form=TargetForm()
